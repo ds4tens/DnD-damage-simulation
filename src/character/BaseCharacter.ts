7845088a97cabb798d602806afc903af6b391b4a
kackom <d.r.lasuria@gmail.com>
2026-03-02 21:17:02 +0300

    add stats, refactor hit chance damage bonus

--- src/character/BaseCharacter.ts
+++ src/character/BaseCharacter.ts
@@ -7,16 +7,64 @@ type Spell = {
     name: string
 }
 
+type StatsTypes = 'strength' | 'dexterity' | 'constitution' | 'intelligence' | 'wisdom' | 'charisma'
+
+type StatBlock = {
+    [key in StatsTypes]: number
+}
+
+const defaultStatBlock: StatBlock = {
+    strength: 10,
+    dexterity: 10,
+    constitution: 10,
+    intelligence: 10,
+    wisdom: 10,
+    charisma: 10
+}
 class BaseCharacter {
 
     _level: number
     _characterClass: BaseClass
     _weapon: Weapon
+    _weaponPrimaryStat: StatsTypes
+    _statBlock: StatBlock
 
-    constructor(level: number, characterClass: BaseClass, weapon: Weapon) {
+    constructor(level: number, characterClass: BaseClass, weapon: Weapon, weaponPrimaryStat: StatsTypes = 'strength', statBlock: StatBlock = defaultStatBlock) {
         this._level = level
         this._characterClass = characterClass
         this._weapon = weapon;
+        this._weaponPrimaryStat = weaponPrimaryStat;
+        this._statBlock = statBlock;
+    }
+
+    getBonusProficiency(): number {
+        if (this._level <= 4) return 2;
+        if (this._level <= 8) return 3;
+        if (this._level <= 12) return 4;
+        if (this._level <= 16) return 5;
+        if (this._level <= 20) return 6;
+        throw new Error('Problem with level')
+    }
+
+    calculateStatModifier(stat: StatsTypes): number {
+        return Math.floor((this._statBlock[stat] - 10) / 2)
+    }
+
+    calculateHitBonus(): number {
+        let hitBonus = 0;
+        const hitStatModifier = this.calculateStatModifier(this._weaponPrimaryStat)
+        hitBonus += hitStatModifier
+        if (this._characterClass._weaponProficiency.find(weapon => weapon.name === this._weapon.name)) {
+            hitBonus += this.getBonusProficiency();
+        }
+        return hitBonus;
+    }
+    
+    calculateDamageBonus(): number {
+        let damageBonus = 0;
+        const damageStatModifier = this.calculateStatModifier(this._weaponPrimaryStat)
+        damageBonus += damageStatModifier
+        return damageBonus;
     }
 
     attack(): {
@@ -24,11 +72,21 @@ class BaseCharacter {
         damage: number,
         damageType: string
     } {
-        let hitBonus = 0;
-        if (this._characterClass._weaponProficiency.find(weapon => weapon.name === this._weapon.name)) hitBonus = 2;
+        const hitBonus = this.calculateHitBonus();
+        const damageBonus = this.calculateDamageBonus();
 
         const hitChance = new Dice(20).rollWithNormalDistribution() + hitBonus
-        const damage = this._weapon.damage.reduce((acc, dice) => acc + dice.rollWithNormalDistribution(), 0)
+        // TODO: make crit chance
+        // TODO: make crit damage
+        // TODO: make dynamic AC based on level or parametr
+        if (hitChance <= 15) return {
+            hitChance: hitChance,
+            damage: 0,
+            damageType: ''
+        }
+
+        
+        const damage = this._weapon.damage.reduce((acc, dice) => acc + dice.rollWithNormalDistribution(), 0) + damageBonus
         const damageType = this._weapon.damageType
 
         // console.log('hitBonus : ', hitBonus)
