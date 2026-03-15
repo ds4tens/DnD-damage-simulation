b7d7506e024b1036811f5d45d7aeeed55ec16c21
kackom <d.r.lasuria@gmail.com>
2026-03-15 18:37:29 +0300

    refactor character creation

--- src/index.ts
+++ src/index.ts
@@ -14,32 +14,61 @@ const heavySword = new Weapon(
     "heavySword", "", "dual", "rare", 100, 300, "big", [d6, d6], "hex"
 )
 
-const barb = new Barbarian(1, [dualSword, heavySword])
+const barb = new Barbarian([dualSword, heavySword])
 
-const Jhon = new BaseCharacter(1, barb, heavySword, 'strength', {
+const stats = {
     strength: 18,
     dexterity: 10,
     constitution: 10,
     intelligence: 10,
     wisdom: 10,
     charisma: 10
-})
-const Emual = new BaseCharacter(20, barb, heavySword, 'dexterity', {
-    strength: 10,
-    dexterity: 18,
-    constitution: 10,
-    intelligence: 10,
-    wisdom: 10,
-    charisma: 10
-})
-
-const jhonDamage = []
-const emualDamage = []
-
-for (let i = 0; i < 10000; i++) {
-    jhonDamage.push(Jhon.attack().damage)
-    emualDamage.push(Emual.attack().damage)
 }
 
-console.log('Jhon avg damage ', jhonDamage.reduce((acc,damage) => acc + damage, 0)/10000)
-console.log('Emual avg damage', emualDamage.reduce((acc, damage) => acc + damage, 0)/10000)
+const listOfCharactersDualSword = Array.from({length: 20}, (_, index) => new BaseCharacter(index + 1, barb, dualSword, 'strength', stats))
+const listOfCharactersHeavySword = Array.from({length: 20}, (_, index) => new BaseCharacter(index + 1, barb, heavySword, 'strength', stats))
+
+
+for (const character of listOfCharactersDualSword) {
+    const damage = []
+    for (let i = 0; i < 1000; i++) {
+        damage.push(character.attack())
+    }
+    console.log(`${character._level} avg damage ${damage.reduce((acc, damage) => acc + damage, 0)/1000}`)
+}
+
+for (const character of listOfCharactersHeavySword) {
+    const damage = []
+    for (let i = 0; i < 1000; i++) {
+        damage.push(character.attack())
+    }
+    console.log(`${character._level} avg damage ${damage.reduce((acc, damage) => acc + damage, 0)/1000}`)
+}
+
+// const Jhon = new BaseCharacter(1, barb, heavySword, 'strength', {
+//     strength: 18,
+//     dexterity: 10,
+//     constitution: 10,
+//     intelligence: 10,
+//     wisdom: 10,
+//     charisma: 10
+// })
+// const Emual = new BaseCharacter(20, barb, heavySword, 'dexterity', {
+//     strength: 10,
+//     dexterity: 18,
+//     constitution: 10,
+//     intelligence: 10,
+//     wisdom: 10,
+//     charisma: 10
+// })
+
+// const jhonDamage = []
+// const emualDamage = []
+
+// for (let i = 0; i < 10000; i++) {
+//     jhonDamage.push(Jhon.attack().damage)
+//     emualDamage.push(Emual.attack().damage)
+// }
+
+// console.log('Jhon avg damage ', jhonDamage.reduce((acc,damage) => acc + damage, 0)/10000)
+// console.log('Emual avg damage', emualDamage.reduce((acc, damage) => acc + damage, 0)/10000)
